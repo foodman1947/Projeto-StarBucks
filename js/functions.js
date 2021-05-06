946cde523e5c5a0e31942df1169c5bef575066ff
gabriel <lemento.tp@gmail.com>
2021-05-06 14:22:14 -0300

    header responsivo com amburguer

--- js/functions.js
+++ js/functions.js
@@ -25,3 +25,45 @@ img3.addEventListener('click',function(){
     gugu.style.marginTop = '-80px';
 
 });
+
+
+/* amburguer *********************/
+
+let ambur = document.querySelector('div.amburguer');
+var barra1 = document.querySelector('div.barra1'), barra2 = document.querySelector('div.barra2');
+var submenu = document.querySelector('div.submenu');
+var ule = document.querySelector('ul.sumenu');
+var lala = false;
+ambur.addEventListener('click',function(){
+    if(lala == false){
+        barra1.style.transition = '0.1s linear';
+        barra2.style.transition = '0.1s linear';
+        barra1.style.transform = "rotate(-50deg)"
+        barra1.style.top = '20px';
+        barra1.style.left = '3px';
+        barra2.style.transform = "rotate(50deg)"
+        barra2.style.top = '15px';
+        lala = true;
+        submenu.style.marginTop = '50px';
+        submenu.style.width = '90%';
+        submenu.style.height = '100px';
+        window.setTimeout(function() {
+            ule.style.display = 'inline-block';
+        }, 200);
+        
+
+    }else{
+        barra1.style.transform = "rotate(0deg)"
+        barra1.style.top = '10px';
+        barra1.style.left = '0px';
+        barra2.style.transform = "rotate(0deg)"
+        barra2.style.top = '25px';
+        submenu.style.width = '0%';
+        submenu.style.height = '0px';
+        ule.style.display = 'none';
+        lala = false;
+
+    };
+        
+     
+});
